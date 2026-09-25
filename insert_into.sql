8c0cadf9ba8d2ee3beadddbfa3052999080b9ae2
Hefot Ahmed <hefot6@gmail.com>
2026-09-25 01:57:30 -0400

    Add initial data for companies table

---
+++ insert_into.sql
@@ -0,0 +1,11 @@
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (1, 'Twitter', 'San Francisco', 2006);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (2, 'Duolingo', 'Pittsburgh 🐝', 2011);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (4, 'Codedex', 'New York 🗽', 2022);
+
+SELECT *
+FROM companies;
